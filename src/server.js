054c61c2bc447e3561081f32b936d5fe053604ea
Rafael Brito <rafaelconcept@hotmail.com>
2020-04-07 18:38:42 -0300

    feito backend e frontend web

---
+++ src/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+
+
+const server = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-wagi7.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true
+});
+
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+
+
+server.listen(3333);
